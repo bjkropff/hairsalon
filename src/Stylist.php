c844563fb09d11b0041150b8cada4de2703f9a96
brian <brian.kropff@gmail.com>
2015-03-20 10:54:00 -0700

    Add save test.

--- src/Stylist.php
+++ src/Stylist.php
@@ -3,23 +3,40 @@
     class Stylist
     {
         private $name;
-        private $id
+    //    private $id
 
-        function __construct($name, $id)
+        function __construct($name)//, $id)
         {
-        $this->name = $name;
-        $this->is = $id;
+            $this->name = $name;
+        //    $this->is = $id;
         }
 
-        function setName($name)
+        function setName($new_name)
         {
-            
+            $this->name = (string) $new_name;
         }
 
         function getName()
         {
-
+            return $this->name;
         }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec('INSERT INTO stylist (name) VALUES ("{$this->getName()}")');
+        }
+
+        // static function getAll()
+        // {
+        //     $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
+        //     $stylists = array();
+        //     foreach($returned_stylist as $one_stylist){
+        //         $name = $one_stylist['name'];
+        //         $new_stylist = new Stylist($name);
+        //         array_push($stylists, $new_stylist);
+        //     }
+        //     return $stylists;
+        //}
     }
 
 ?>
--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -1,20 +1,44 @@
 <?php
 
-    require_once "src/MyProjectClass.php";
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+
+    require_once "src/Stylist.php";
+
+    $DB = new PDO('pgsql:host=localhost;dbname=hair_salon_test');
 
     class StylistTest extends PHPUnit_Framework_TestCase
     {
-        function test_myFunction_firstTest()
+        function test_get()
         {
             //Arrange
-            $test_MyProjectClass = new MyProjectClass;
+            $name = "Bob";
+            $test_get_name = new Stylist($name);
 
             //Act
-            $result = $test_MyProjectClass->myFunction();
+            $result = $test_get_name->getName();
 
             //Assert
-            $this->assertEquals( /* expectation */, $result);
+            $this->assertEquals($test_get_name, $result);
+
         }
+
+        // function test_save()
+        // {
+        //     //Arrange
+        //     $name = "Bob";
+        //     $test_get_name = new Stylist($name);
+        //
+        //     //Act
+        //     $test_get_name->save();
+        //
+        //     //Assert
+        //     $result = Stylist::getAll();
+        //     $this->assertEquals($test_get_name, $result);
+        // }
     }
 
 ?>
