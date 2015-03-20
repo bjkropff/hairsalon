edc2f2255e69039dd7ad9cd7c7b468a5e68f0e25
brian <brian.kropff@gmail.com>
2015-03-20 10:12:50 -0700

    Add initial files

---
+++ src/Stylist.php
@@ -0,0 +1,25 @@
+<?php
+
+    class Stylist
+    {
+        private $name;
+        private $id
+
+        function __construct($name, $id)
+        {
+        $this->name = $name;
+        $this->is = $id;
+        }
+
+        function setName($name)
+        {
+            
+        }
+
+        function getName()
+        {
+
+        }
+    }
+
+?>
